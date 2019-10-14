3971c47c719e939b2622bb440358392b1501e30f
vamshicloudpanel <vamshik@cloudpaneltec.com>
2019-10-14 14:59:02 +0530

    removed popular offers

--- src/views/Offers/components/Categories/CategoryCard/CategoryCard.js
+++ src/views/Offers/components/Categories/CategoryCard/CategoryCard.js
@@ -15,13 +15,13 @@ export default class CategoryCard extends Component {
 
                 <a href={"/offers?cat_id="+this.props.id+"&categoryName="+this.props.name}>
                     <div className="col s6 m4 no-left-padding categoryCardWrapper">
-                        <a href="category.html" className="valign-wrapper">
+                        
                             <div className="lighten-1 valign-wrapper categoryCard"
                                  style={{backgroundColor: "rgb(23, 43, 84)"}}>
                                 <img src={myConfig.apiBackendUrl+'/media/'+this.props.icon}
                                      className="responsive-img center-block" width={"60"} height={"60"}/>
                             </div>
-                        </a>
+
                         <div className="categoryTitle center-align" style={{color: "rgb(23, 43, 84)"}}>{this.props.name}</div>
                     </div>
                 </a>
